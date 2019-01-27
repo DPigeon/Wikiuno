6f391315a438b080853a437cca0bf2d6078e7402
DPigeon <D_Pig@encs.concordia.ca>
2019-01-27 03:58:09 -0500

    Adding the path module
    Needed a module to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const path = require("path");
 
 // API Data
 const DatasAPI = require("./models/data");
